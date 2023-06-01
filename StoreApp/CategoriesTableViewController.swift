635215657314a197469e6cedd3c42e7ab0f32585
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-06-01 10:59:02 +0700

    Init Store App Project

---
+++ StoreApp/CategoriesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by admin on 01/06/2023.
+//
+
+import UIKit
+
+class CategoriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
---
+++ StoreApp/Models/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  StoreApp
+//
+//  Created by admin on 01/06/2023.
+//
+
+import Foundation
+
+struct Category: Codable, Hashable{
+    let id: Int
+    let name: String
+    let imgae: String
+}
