faf27bd6636e491c4a0ebe9171373b5067a5c2b7
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-06-12 17:48:58 +0700

    Implementing generic network layer in storeHttpClient

--- StoreApp/Clients/StoreHTTPClient.swift
+++ StoreApp/Clients/StoreHTTPClient.swift
@@ -13,7 +13,68 @@ enum NetworkError: Error {
     case decodingError
 }
 
+enum HttpMethod {
+    case get([URLQueryItem])
+    case post(Data?)
+    case delete
+    
+    var name: String {
+        switch self {
+        case .get:
+            return "GET"
+        case .delete:
+            return "DELETE"
+        case .post:
+            return "POST"
+        }
+    }
+}
+struct Resource<T:Codable> {
+    let url: URL
+    var headers: [String: String] = [:]
+    var method: HttpMethod = .get([])
+}
+
 class StoreHTTPClient {
+    
+    func load<T:Codable>(_ resource: Resource<T>) async throws -> T {
+        var request = URLRequest(url: resource.url)
+        
+        switch resource.method {
+        case .get(let queryItems):
+            var components = URLComponents(url: resource.url, resolvingAgainstBaseURL: true)
+            components?.queryItems = queryItems
+            guard let url = components?.url else {
+                throw NetworkError.invalidURL
+            }
+            
+            request = URLRequest(url: url)
+        case .post(let data):
+            request.httpBody = data
+        default: break
+        }
+        
+        request.allHTTPHeaderFields = resource.headers
+        request.httpMethod = resource.method.name
+        
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = ["Content-Type":"application/json"]
+        
+        let session = URLSession(configuration: configuration)
+        
+        let (data, response) = try await session.data(for: request)
+        guard let _ = response as? HTTPURLResponse
+        else {
+            throw NetworkError.invalidServerResponse
+        }
+        
+        guard let result = try? JSONDecoder().decode(T.self, from: data) else {
+            throw NetworkError.decodingError
+        }
+        
+        return result
+                
+    }
     
     func getProductByCategory(categoryId: Int) async throws -> [Product] {
         let (data, response) = try await URLSession.shared.data(from: URL.productsByCategory(categoryId))
